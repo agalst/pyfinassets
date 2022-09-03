9d1a14a2baae57eafc3e442d349af547a4a531fb
agalsty&outlook.fr <agalsty&outlook.fr>
2022-09-03 22:06:33 +0000

    Add pendulum and dummy functions for bonds

--- src/assets/financialassetbond.py
+++ src/assets/financialassetbond.py
@@ -1,5 +1,12 @@
+from pendulum import DateTime
 from src.abstracts.abstractfinancialasset import AbstractFinancialAsset
 
 class FinancialAssetBond(AbstractFinancialAsset):
     def __init__(self):
+        pass
+
+    def accrue_interest(self, until_date: DateTime):
+        pass
+
+    def list_coupons(self):
         pass
