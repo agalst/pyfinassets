cd4a99950e18e236def4eef9f9e9e10927ae871d
agalsty&outlook.fr <agalsty&outlook.fr>
2022-09-03 14:19:13 +0000

    Add CFICode to security abstract

--- src/abstracts/abstractfinancialasset.py
+++ src/abstracts/abstractfinancialasset.py
@@ -1,6 +1,8 @@
 from abc import ABC
+from src.cficode import CFICode
 
 class AbstractFinancialAsset(ABC):
     securityName: str
     securityISIN: str
     securityExchangeMIC: str
+    securityCFICode : CFICode
