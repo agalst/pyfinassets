9576339c8b1dd76faf4735edd945ea5b84f98e11
agalsty&outlook.fr <agalsty&outlook.fr>
2022-09-03 14:17:30 +0000

    More CFI code logic

--- src/cficode.py
+++ src/cficode.py
@@ -1,9 +1,10 @@
-from collections import namedtuple
+from src.const import cfi_classes, cfi_types
 
 class CFICode:
     code: str = ""
 
-    def __init__(self):
+    def __init__(self, code:str):
+        self.code = code
         self.first_char = self.code[0]
         self.second_char = self.code[1]
         self.third_char = self.code[2]
@@ -12,19 +13,19 @@ class CFICode:
         self.sixth_char = self.code[5]
 
     def get_asset_class(self):
+        return cfi_classes.get(self.first_char)
+
+    def get_asset_type(self):
+        return cfi_types.get(self.first_char + self.second_char)
+
+    def get_asset_attribute1(self):
         pass
 
-    def get_asset_type():
+    def get_asset_attribute2(self):
         pass
 
-    def get_asset_class():
+    def get_asset_attribute3(self):
         pass
 
-    def get_asset_class():
-        pass
-
-    def get_asset_class():
-        pass
-
-    def get_asset_class():
+    def get_asset_attribute4(self):
         pass
