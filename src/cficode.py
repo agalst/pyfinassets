70e30ae3ae49d38dc82128d6c55a680d6558a67e
agalsty&outlook.fr <agalsty&outlook.fr>
2022-09-03 13:55:29 +0000

    Add CFI code dummy class

---
+++ src/cficode.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+
+class CFICode:
+    code: str = ""
+
+    def __init__(self):
+        self.first_char = self.code[0]
+        self.second_char = self.code[1]
+        self.third_char = self.code[2]
+        self.fourth_char = self.code[3]
+        self.fifth_char = self.code[4]
+        self.sixth_char = self.code[5]
+
+    def get_asset_class(self):
+        pass
+
+    def get_asset_type():
+        pass
+
+    def get_asset_class():
+        pass
+
+    def get_asset_class():
+        pass
+
+    def get_asset_class():
+        pass
+
+    def get_asset_class():
+        pass
