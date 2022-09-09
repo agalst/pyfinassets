7c3792cf4ecc402573aa846d049d37c9f25fca3c
agalsty&outlook.fr <agalsty&outlook.fr>
2022-09-09 22:06:40 +0000

    apple metadata

--- src/example.py
+++ src/example.py
@@ -4,7 +4,7 @@ from src.assetfactory import FinancialAssetFactory
 
 assetFactory = FinancialAssetFactory()
 
-appleSecurity = assetFactory.create_asset(cfi_code="", name="", isin="", mic="")
+appleSecurity = assetFactory.create_asset(cfi_code="ESVUFR", name="Apple Inc. Common Stock (AAPL)", isin="US0378331005", mic="XNAS")
 
 myPortfolio = Portfolio()
 myApplePosition = Position(asset=appleSecurity, quantity=10)
