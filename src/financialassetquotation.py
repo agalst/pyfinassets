52998099cc247469f57bf062aa557ab2d3e08871
agalsty&outlook.fr <agalsty&outlook.fr>
2022-09-09 21:28:37 +0000

    Add init

--- src/financialassetquotation.py
+++ src/financialassetquotation.py
@@ -2,4 +2,8 @@ from pendulum import DateTime
 
 class FinancialAssetQuotation:
     price: float
-    quontationTimestamp: DateTime
+    quotationTimestamp: DateTime
+
+    def __init__(self, timestamp: DateTime, price: float):
+        self.price = price
+        self.quotationTimestamp = timestamp
