1e320f88cccfb9716843a382025bf1ec89696d46
йпagalsty <agalsty@outlook.fr>
2022-10-29 21:12:09 +0000

    Bring asset init to the abstract class

--- src/abstracts/abstractfinancialasset.py
+++ src/abstracts/abstractfinancialasset.py
@@ -8,6 +8,14 @@ class AbstractFinancialAsset(ABC):
     securityISIN: str
     securityExchangeMIC: str
     securityCFICode: CFICode
+    securitySymbol: str
+
+    def __init__(self, name, isin, mic, cfi_code, symbol):
+        self.securityCFICode = cfi_code
+        self.securityExchangeMIC = mic
+        self.securityISIN = isin
+        self.securityName = name
+        self.securitySymbol = symbol
 
     def __str__(self):
         return (
