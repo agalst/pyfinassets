73e8c67e2853495f756914281184d5394102a8e1
йпagalsty <agalsty@outlook.fr>
2022-10-29 21:13:27 +0000

    Update example

--- src/example.py
+++ src/example.py
@@ -1,18 +1,33 @@
+from pendulum import DateTime
+
+from assetfactory import FinancialAssetFactory
 from portfolio import Portfolio
 from position import Position
-from assetfactory import FinancialAssetFactory
 
 assetFactory = FinancialAssetFactory()
 
-appleSecurity = assetFactory.create_asset(cfi_code="ESVUFR", 
-                                          name="Apple Inc. Common Stock (AAPL)", 
-                                          isin="US0378331005", 
-                                          mic="XNAS")
+appleSecurity = assetFactory.create_asset(
+    cfi_code="ESVUFR",
+    name="Apple Inc. Common Stock (AAPL)",
+    isin="US0378331005",
+    mic="XNAS",
+    symbol="AAPL",
+)
 
 myPortfolio = Portfolio()
-myApplePosition = Position(asset=appleSecurity, quantity=10)
+myApplePosition = Position(
+    asset=appleSecurity, quantity=10, date=DateTime(2022, 10, 28)
+)
+myApplePosition1 = Position(
+    asset=appleSecurity, quantity=10, date=DateTime(2022, 10, 3)
+)
 
 
 myPortfolio.addPosition(myApplePosition)
+myPortfolio.addPosition(myApplePosition1)
+myPortfolio.update_prices()
 print(myPortfolio)
-print(myPortfolio.evaluate())
+evaluation_date = DateTime(2022, 10, 4)
+value = myPortfolio.evaluate(date=evaluation_date)
+str_date = evaluation_date.strftime("%Y-%m-%d")
+print(f"On {str_date} my portfolio is worth {value}$")
