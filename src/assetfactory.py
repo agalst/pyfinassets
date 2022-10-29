e212478a95af90f414efc177760bff300587b428
йпagalsty <agalsty@outlook.fr>
2022-10-29 20:08:44 +0000

    Change asset factory creation method, raise exception if asset class not found

--- src/assetfactory.py
+++ src/assetfactory.py
@@ -1,12 +1,20 @@
-from assets.financialassetequity import FinancialAssetEquity
 from assets.financialassetdebt import FinancialAssetDebt
+from assets.financialassetequity import FinancialAssetEquity
 from cficode import CFICode
 
-class FinancialAssetFactory:
 
-    def create_asset(self, cfi_code: str="", name: str="", isin: str="", mic: str=""):
+class FinancialAssetFactory:
+    def create_asset(
+        self, cfi_code: str = "", name: str = "", isin: str = "", mic: str = ""
+    ):
         asset_cfi_code = CFICode(cfi_code)
-        if asset_cfi_code.get_asset_class() == "E":
-            return FinancialAssetEquity(name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic)
-        elif asset_cfi_code.get_asset_class() == "D":
-            return FinancialAssetDebt(name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic)
+        if asset_cfi_code.get_asset_class() == "Equity":
+            return FinancialAssetEquity(
+                name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic
+            )
+        elif asset_cfi_code.get_asset_class() == "Debt":
+            return FinancialAssetDebt(
+                name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic
+            )
+        else:
+            raise NotImplementedError("This asset class is not implemented yet!")
