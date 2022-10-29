926a3a98fdd0289a66de0f0384134028e9ef849d
йпagalsty <agalsty@outlook.fr>
2022-10-29 21:12:35 +0000

    Remove inits from implementations

--- src/assets/financialassetdebt.py
+++ src/assets/financialassetdebt.py
@@ -1,13 +1,9 @@
 from pendulum import DateTime
+
 from abstracts.abstractfinancialasset import AbstractFinancialAsset
 
-class FinancialAssetDebt(AbstractFinancialAsset):
-    def __init__(self, name, isin, mic, cfi_code):
-        self.securityCFICode = cfi_code
-        self.securityExchangeMIC = mic
-        self.securityISIN = isin
-        self.securityName = name
 
+class FinancialAssetDebt(AbstractFinancialAsset):
     def accrue_interest(self, until_date: DateTime):
         pass
 
--- src/assets/financialassetequity.py
+++ src/assets/financialassetequity.py
@@ -1,8 +1,5 @@
 from abstracts.abstractfinancialasset import AbstractFinancialAsset
 
+
 class FinancialAssetEquity(AbstractFinancialAsset):
-    def __init__(self, name, isin, mic, cfi_code):
-        self.securityCFICode = cfi_code
-        self.securityExchangeMIC = mic
-        self.securityISIN = isin
-        self.securityName = name
+    pass
