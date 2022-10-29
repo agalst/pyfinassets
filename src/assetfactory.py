92f61f3686dc1433d83da2fc16abf6c703d5499c
йпagalsty <agalsty@outlook.fr>
2022-10-29 21:11:44 +0000

    Use security symbol to fetch quotation

--- src/assetfactory.py
+++ src/assetfactory.py
@@ -5,16 +5,21 @@ from cficode import CFICode
 
 class FinancialAssetFactory:
     def create_asset(
-        self, cfi_code: str = "", name: str = "", isin: str = "", mic: str = ""
+        self,
+        cfi_code: str = "",
+        name: str = "",
+        isin: str = "",
+        mic: str = "",
+        symbol: str = "",
     ):
         asset_cfi_code = CFICode(cfi_code)
         if asset_cfi_code.get_asset_class() == "Equity":
             return FinancialAssetEquity(
-                name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic
+                name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic, symbol=symbol
             )
         elif asset_cfi_code.get_asset_class() == "Debt":
             return FinancialAssetDebt(
-                name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic
+                name=name, isin=isin, cfi_code=asset_cfi_code, mic=mic, symbol=symbol
             )
         else:
             raise NotImplementedError("This asset class is not implemented yet!")
